032ed3c03179a13fe2fbd79be0f359edaeac8895
Siegbaert <Siegbaert@users.noreply.github.com>
2021-03-09 21:55:50 +0100

    Enforcing DLT storaged file & refactoring (#2)
    * Enforce storaged DLT file, otherwise raise error
    
    * Minor refactoring

--- dlt_transformipy/core/model/dlt_message.py
+++ dlt_transformipy/core/model/dlt_message.py
@@ -1,17 +1,17 @@
 # MIT License
-# 
+#
 # Copyright (c) 2021 Dennis Schwarz
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -31,14 +31,13 @@ class DLTMessage:
     extended_header = None
     payload = None
 
-    def __init__(self, dlt_message_hex, is_storaged_file):
+    def __init__(self, dlt_message_hex):
         # Always points to the starting byte of the next info to read from dlt_message_hex
         start_byte_pointer = 0
-        # Read the storage-header, if applicable
-        if is_storaged_file:
-            self.storage_header = StorageHeader(dlt_message_hex, start_byte_pointer)
-            # Update the start byte pointer (move it to the end of STORAGE_HEADER)
-            start_byte_pointer += self.storage_header.get_byte_size() * 2
+        # Read the storage-header
+        self.storage_header = StorageHeader(dlt_message_hex, start_byte_pointer)
+        # Update the start byte pointer (move it to the end of STORAGE_HEADER)
+        start_byte_pointer += self.storage_header.get_byte_size() * 2
 
         # Read the standard-header
         self.standard_header = StandardHeader(dlt_message_hex, start_byte_pointer)
